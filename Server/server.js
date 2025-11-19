b9c5d0c2d739b9f31dcecbd519d362c2375f0445
SAMSAMETH <18samuelstephen@gmail.com>
2025-11-19 14:26:50 +0530

    ok

--- Server/server.js
+++ Server/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.send("API LIVE"));
 
 app.get("/", (req, res) => {
   res.send("Razorpay Backend Running 🚀");
